41326b9d31c0a2762f35aadbc0947d6a592c141d
kingyj00 <onshopyj@gmail.com>
2025-01-10 17:39:41 +0900

    RootUrl-UseService-Q,A(2-12)

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping("/start")
     @ResponseBody
     public String index() {
         return "프로젝트 시작.";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
     }
 }
